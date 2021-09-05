ef32a1cf8fe257d59f9e3017093c1e10a7b15113
pskvins <pskvins@snu.ac.kr>
2021-09-05 18:27:18 +0900

    differentiation of matrix done + gradient, fixing of adding job in next_iteration in expectation step needs to be done

--- maximization_step.cpp
+++ maximization_step.cpp
@@ -13,6 +13,7 @@ void calculate_derivative(gsl_matrix *qmatrix, double *codon_freq, gsl_matrix *e
     for (int num = 0; num < 64 * 63 / 2; num++) {
         dxepon_matrix[num] = gsl_matrix_alloc(64, 64);
     }
+    double grad[64 * 63 / 2] = {0.0};
     double diagonal[64] = {0};
     double normalize;
     for (int row = 0; row < 64; row++) {
@@ -26,30 +27,29 @@ void calculate_derivative(gsl_matrix *qmatrix, double *codon_freq, gsl_matrix *e
     #pragma omp parallel for schedule(dynamic)
     for (size_t diff = 0; diff < 64 * 63 / 2; diff++) {
         thread_local std::vector<newick_graph*> next_iteration = start->next;
-        thread_local gsl_matrix *dx_matrix = gsl_matrix_alloc(64, 64);//freed
         thread_local gsl_matrix *F = gsl_matrix_alloc(64, 64);//freed
         thread_local size_t row_tar = num_to_coordinate.find(diff)->second.begin()->first;
         thread_local size_t col_tar = num_to_coordinate.find(diff)->second.begin()->second;
-        gsl_matrix_set_all(dx_matrix, 0.0);
         while (next_iteration.size() != 1) {
             size_t size = next_iteration.size();
             for (size_t num = 0; num < size; num++) {
+                gsl_matrix_set_all(dxepon_matrix[diff], 0.0);
                 for (size_t row = 0; row < 64; row++) {
                     for (size_t col = 0; col < 64; col++) {
                         if (row == col) {
-                            gsl_matrix_set(dx_matrix, row, col,
+                            gsl_matrix_set(dxepon_matrix[diff], row, col,
                                            (2 * codon_freq[row_tar] * codon_freq[col_tar] * diagonal[row] - codon_freq[col_tar] * normalize) / pow(normalize, 2));
                         } else if ((row == row_tar && col == col_tar) || (row == col_tar && col == row_tar)) {
-                            gsl_matrix_set(dx_matrix, row, col,
+                            gsl_matrix_set(dxepon_matrix[diff], row, col,
                                            (codon_freq[col] * normalize - 2 * codon_freq[row] * pow(codon_freq[col], 2) * gsl_matrix_get(qmatrix, row_tar, col_tar) / pow(normalize, 2)));
                         } else {
-                            gsl_matrix_set(dx_matrix, row, col,
+                            gsl_matrix_set(dxepon_matrix[diff], row, col,
                                            -gsl_matrix_get(qmatrix, row, col) * 2 * codon_freq[row_tar] * codon_freq[col_tar] / pow(normalize, 2));
                         }
                     }
                 }
-                gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, eigen_inverse, dx_matrix, 0.0, dx_matrix);
-                gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, dx_matrix, eigenvector, 0.0, dx_matrix);
+                gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, eigen_inverse, dxepon_matrix[diff], 0.0, dxepon_matrix[diff]);
+                gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, dxepon_matrix[diff], eigenvector, 0.0, dxepon_matrix[diff]);
                 for (size_t row = 0; row < 64; row++) {
                     for (size_t col = 0; col < 64; col++) {
                         if (abs(gsl_vector_get(eigenvalue, row) - gsl_vector_get(eigenvalue, col)) < 1.0e-14) {
@@ -63,12 +63,24 @@ void calculate_derivative(gsl_matrix *qmatrix, double *codon_freq, gsl_matrix *e
                         }
                     }
                 }
-                gsl_matrix_mul_elements(dx_matrix, F);
+                gsl_matrix_mul_elements(dxepon_matrix[diff], F);
                 gsl_matrix_free(F);
-                gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, eigenvector, dx_matrix, 0.0, dx_matrix);
-                gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, dx_matrix, eigen_inverse, 0.0, dxepon_matrix[num]);
-                gsl_matrix_free(dx_matrix);
+                gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, eigenvector, dxepon_matrix[diff], 0.0, dxepon_matrix[diff]);
+                gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, dxepon_matrix[diff], eigen_inverse, 0.0, dxepon_matrix[num]);
+                for (size_t row = 0; row < 64; row++) {
+                    for (size_t col = 0; col < 64; col++) {
+                        gradient[diff] += next_iteration[num]->expectation[64 * row + col]
+                                * gsl_matrix_get(dxepon_matrix[diff], row, col)
+                                / gsl_matrix_get(next_iteration[num]->expon_matrix, row, col);
+                    }
+                }
+                if (std::find(next_iteration.begin(), next_iteration.end(), next_iteration[num]->next) == next_iteration.end()) {
+                    if (next_iteration[num]->next != NULL) {
+                        next_iteration.emplace_back(next_iteration[num]->next);
+                    }
+                }
             }
+            next_iteration.erase(next_iteration.begin(), next_iteration.begin() + size);
         }
     }
 }
